a26d0f6c87dfd6d5ed3f13d77b43145ce9f25c07
SWkeon <121371840+SWkeon@users.noreply.github.com>
2023-02-02 20:16:26 +0900

    Create boj_2293.cpp
    Baekjoon #2293
    try: 1
    solved: 1
    used algorithm: DP

---
+++ Boj_gold/boj_2293.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int n, k;
+int dp[10001];
+int coin[100];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> coin[i];
+	}
+	dp[0] = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = coin[i]; j <= k; j++) {
+			dp[j] += dp[j - coin[i]];
+		}
+	}
+
+	cout << dp[k];
+
+	return 0;
+}
